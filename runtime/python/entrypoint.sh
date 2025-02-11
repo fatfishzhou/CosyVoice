3c4a1435b014c491cf02071daf8221e45b0413d4
周飞宇 <feiyu.2002@outlook.com>
2025-02-11 15:33:00 +0000

    test

--- runtime/python/entrypoint.sh
+++ runtime/python/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 确保在出现错误时退出
-set -e
+# set -e
 
 # 1. 设置模型下载 URL（使用环境变量，保证安全）
 MODEL_SAS_URL=${MODEL_SAS_URL:-"https://aiassistmldevw0107074910.blob.core.windows.net/model-cosyvoice/CosyVoice2-0.5B?sp=rl&st=2025-02-11T14:35:13Z&se=2025-12-31T22:35:13Z&spr=https&sv=2022-11-02&sr=c&sig=04wDlFSHH8KK%2FKqMqtG5fdKdHZ%2BPFuUsPgWm8%2FIQ5nM%3D"}
@@ -30,6 +30,9 @@ fi
 
 # 3. 进入服务端代码目录
 cd /opt/CosyVoice/CosyVoice/runtime/python/grpc
+
+# 重新生成.proto文件
+protoc --python_out=. --grpc_python_out=. -I. cosyvoice.proto
 
 # 4. 启动gRPC服务
 echo "Starting server..."
