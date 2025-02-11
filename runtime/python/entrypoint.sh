ef9b371b2573e28380e5a99414b99828c3916d7e
周飞宇 <feiyu.2002@outlook.com>
2025-02-11 14:52:56 +0000

    test

--- runtime/python/entrypoint.sh
+++ runtime/python/entrypoint.sh
@@ -4,12 +4,12 @@
 set -e
 
 # 1. 设置模型下载 URL（使用环境变量，保证安全）
-MODEL_URL=${MODEL_SAS_URL:-"https://aiassistmldevw0107074910.blob.core.windows.net/model-cosyvoice/CosyVoice2-0.5B?sv=sp=r&st=2025-02-11T12:58:03Z&se=2025-02-28T20:58:03Z&spr=https&sv=2022-11-02&sr=c&sig=pFrTnCBERMSGMb3%2FEHRaNHBAkt5HFEqtNw4FHMxoiTY%3D"}
+MODEL_URL=${MODEL_SAS_URL:-"https://aiassistmldevw0107074910.blob.core.windows.net/model-cosyvoice/CosyVoice2-0.5B?sp=rl&st=2025-02-11T14:35:13Z&se=2025-12-31T22:35:13Z&spr=https&sv=2022-11-02&sr=c&sig=04wDlFSHH8KK%2FKqMqtG5fdKdHZ%2BPFuUsPgWm8%2FIQ5nM%3D"}
 
 echo "[Entrypoint] Starting initialization..."
 # 2. 下载模型文件到工作目录
 echo "[Entrypoint] Downloading model folder with AzCopy..."
-azcopy copy "$MODEL_SAS_URL" "/opt/CosyVoice/CosyVoice2-0.5B" --recursive=true
+azcopy copy "$MODEL_SAS_URL" "/opt/CosyVoice/CosyVoice2-0.5B" --recursive
 
 # 2. 强制更新代码仓库
 echo "Updating source code..."
